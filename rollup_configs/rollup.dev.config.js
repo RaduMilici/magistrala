d5ada3413cc6f05b022d6981c917ab366e332e8a
Radu Milici <radu.milici@3pillarglobal.com>
2021-09-24 21:10:44 +0300

    rollup.dev.config.js uses "rollup-plugin-clean" plugin

--- rollup_configs/rollup.dev.config.js
+++ rollup_configs/rollup.dev.config.js
@@ -1,8 +1,9 @@
 import merge from 'deepmerge';
 import html from '@rollup/plugin-html';
 import serve from 'rollup-plugin-serve';
+import clean from 'rollup-plugin-clean';
 import { rollupBaseConfig } from './rollup.base.config';
 
 export default merge(rollupBaseConfig, {
-  plugins: [html({}), serve('dist')],
+  plugins: [html({}), serve('dist'), clean()],
 });
