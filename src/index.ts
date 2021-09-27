0ad8685efeb21499dec2f2ef80feb2c8235b5351
Radu Milici <radu.milici@3pillarglobal.com>
2021-09-27 09:20:22 +0300

    added index.ts as entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log(1 + 1 + 1);
+import { Renderer } from './core/renderer/Renderer';
+
+const renderer = new Renderer({
+  container: document.getElementById('canvas-container'),
+  size: { width: 500, height: 500 },
+});
+
+console.log(renderer);
