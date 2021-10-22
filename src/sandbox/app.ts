7aee838f1c05242e3388ef47afd6bcebb1c90b1a
Radu Milici <radu.milici@3pillarglobal.com>
2021-10-22 13:29:03 +0300

    - added magistrala-app-ui container - installed and using @rollup/plugin-replace and @rollup/plugin-alias -

--- src/sandbox/app.ts
+++ src/sandbox/app.ts
@@ -2,6 +2,7 @@ import { App } from '../app/App';
 import { Triangle } from '../core/Triangle';
 import fragmentShaderSource from '../shaders/fragment_shader.glsl';
 import { vertexShaderChunks } from '../shaders/vertex_shader_chunks';
+import { createApp, defineComponent } from 'vue';
 
 const app = new App({
   container: document.getElementById('magistrala-app'),
@@ -20,3 +21,16 @@ const mesh = app.newMesh({ vertexShader, fragmentShader, geometry });
 const scene = app.newScene();
 scene.add(mesh);
 app.render(scene);
+
+const Component = defineComponent({
+  methods: {
+    onChange(e: Event) {
+      const { value } = <HTMLInputElement>e.target;
+      console.log(value);
+    },
+  },
+  template:
+    '<input @change="onChange" type="range" min="0" max="100" value="0">',
+});
+
+createApp(Component).mount('#magistrala-app-ui');
