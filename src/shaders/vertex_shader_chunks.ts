601f2cb7603648881d44eda5ef5c0430eb793f2a
Radu Milici <radu.milici@3pillarglobal.com>
2021-10-26 16:43:13 +0300

    removed unused shader code

--- src/shaders/vertex_shader_chunks.ts
+++ src/shaders/vertex_shader_chunks.ts
@@ -6,11 +6,7 @@ export const vertexShaderChunks = `${VERTEX_SHADER_BOILERPLATE}
 void main() {
   fragColor = a_vertColor;
   gl_PointSize = u_pointSize;
-  
-  // vec2 rotatedPosition = getRotatedPosition(a_position, u_rotation);
-  // gl_Position = vec4(rotatedPosition * u_scale + u_translation, 0., 1.);
-  // vec2 position  = (${UniformNames.MATRIX} * vec3(${AttributeNames.POSITION}, 1)).xy;
-  // gl_Position = vec4(position * vec2(1, -1), 0, 1);
+
   gl_Position = ${UniformNames.MATRIX} * ${AttributeNames.POSITION};
 }
 `;
