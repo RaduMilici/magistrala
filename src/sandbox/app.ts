0dbc74846359052bc428d734a04cf60889297f53
Radu Milici <radu.milici@3pillarglobal.com>
2021-10-31 11:30:11 +0200

    objurl enum

--- src/sandbox/app.ts
+++ src/sandbox/app.ts
@@ -12,14 +12,9 @@ import { Color } from '../core/color/Color';
 import { Component3D } from '../core/ecs/Component3D';
 import { GameObject3D } from '../core/ecs/GameObject3D';
 import { ObjLoader } from '../loader/ObjLoader';
+import { ObjUrl } from './assets/obj_url';
 import fragmentShaderSource from './shaders/fragment_shader.glsl';
 import { vertexShaderChunks } from './shaders/vertex_shader_chunks';
-
-enum MeshUrls {
-  CUBE = 'https://gist.githubusercontent.com/MaikKlein/0b6d6bb58772c13593d0a0add6004c1c/raw/48cf9c6d1cdd43cc6862d7d34a68114e2b93d497/cube.obj',
-  TEAPOT = 'https://gist.githubusercontent.com/RaduMilici/ea6f99035120ea0e2dec3d73f72e9061/raw/89295c4f1a948cfba5f99fe0abd95ba61ec21271/teapot.obj',
-  TEDDY = 'https://gist.githubusercontent.com/RaduMilici/07c8571799e68da7aa5d051bbcca6e32/raw/7d1005611653859231725a5b420d57702cece954/teddy.obj',
-}
 
 const rendererSize: size = {
   width: window.innerWidth,
@@ -48,7 +43,7 @@ class FShape extends GameObject3D {
     const fragmentShader = app.newFragmentShader({
       source: fragmentShaderSource,
     });
-    const { triangles } = await new ObjLoader().load(MeshUrls.TEDDY);
+    const { triangles } = await new ObjLoader().load(ObjUrl.TEDDY);
     triangles.forEach((triangle) => (triangle.color = Color.random()));
     this.mesh = app.newMesh({
       fragmentShader,
