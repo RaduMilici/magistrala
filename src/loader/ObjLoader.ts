f8528f493fc6287584802db1e1db654f4a6b19e9
Radu Milici <radu.milici@3pillarglobal.com>
2021-11-02 17:30:51 +0200

    simplified loader code and added ObjDelimiters

--- src/loader/ObjLoader.ts
+++ src/loader/ObjLoader.ts
@@ -2,7 +2,7 @@ import { Vector3 } from '../core/Vector3';
 import { Triangle } from '../core/triangle/Triangle';
 import { MeshData } from './MeshData';
 import { MeshCache, PromiseCache, TextCache } from './loader-caches';
-import { ObjSpecifiers } from './obj-specifiers';
+import { ObjDelimiters, ObjSpecifiers } from './obj-specifiers';
 
 export class ObjLoader {
   public static promiseCache: PromiseCache = {};
@@ -13,14 +13,11 @@ export class ObjLoader {
     if (ObjLoader.promiseCache.hasOwnProperty(path)) {
       return ObjLoader.returnFromCache(path);
     }
-
     ObjLoader.promiseCache[path] = fetch(path);
-    const response = await ObjLoader.promiseCache[path];
-
-    ObjLoader.textCache[path] = response.text();
-    const text = await ObjLoader.textCache[path];
-
-    ObjLoader.meshCache[path] = ObjLoader.readStringData(text);
+    ObjLoader.textCache[path] = (await ObjLoader.promiseCache[path]).text();
+    ObjLoader.meshCache[path] = ObjLoader.readStringData(
+      await ObjLoader.textCache[path],
+    );
     return ObjLoader.meshCache[path];
   }
 
@@ -32,9 +29,9 @@ export class ObjLoader {
 
   private static readStringData(text: string): MeshData {
     const meshData = new MeshData();
-    const rows = text.split('\n');
+    const rows = text.split(ObjDelimiters.ROW);
     rows.forEach((element) => {
-      const [specifier, ...values] = element.split(' ');
+      const [specifier, ...values] = element.split(ObjDelimiters.VALUES);
       switch (specifier) {
         case ObjSpecifiers.VERTEX:
           ObjLoader.assignVertex(values, meshData);
@@ -61,14 +58,13 @@ export class ObjLoader {
     const vertexIndices: Array<number> = [];
     triangleIndices.forEach((index) => {
       // TODO: implement textureCoord and normal once supported
-      const [position, textureCoord, normal] = index.split('/').map(parseFloat);
+      const [position, textureCoord, normal] = index
+        .split(ObjDelimiters.INDICES)
+        .map(parseFloat);
       // subtracting 1 because .obj indices are not 0 based
       vertexIndices.push(position - 1);
     });
-    const [aIndex, bIndex, cIndex] = vertexIndices;
-    const a = meshData.vertices[aIndex];
-    const b = meshData.vertices[bIndex];
-    const c = meshData.vertices[cIndex];
+    const [a, b, c] = vertexIndices.map((i) => meshData.vertices[i]);
     meshData.triangles.push(new Triangle({ a, b, c }));
   }
 }
--- src/loader/obj-specifiers.ts
+++ src/loader/obj-specifiers.ts
@@ -2,3 +2,9 @@ export enum ObjSpecifiers {
   VERTEX = 'v',
   FACE = 'f',
 }
+
+export enum ObjDelimiters {
+  ROW = '\n',
+  VALUES = ' ',
+  INDICES = '/',
+}
