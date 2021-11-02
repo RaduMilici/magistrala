e9fa0383f3851869f56f9297f58d760125b2a72b
Radu Milici <radu.milici@3pillarglobal.com>
2021-11-02 17:18:34 +0200

    Vector3 has optional params as coords, defaults to zero

--- src/core/Vector3.ts
+++ src/core/Vector3.ts
@@ -4,7 +4,11 @@ export class Vector3 {
   z: number = 0;
 
   constructor(
-    { x, y, z }: { x: number; y: number; z: number } = { x: 0, y: 0, z: 0 },
+    { x = 0, y = 0, z = 0 }: { x?: number; y?: number; z?: number } = {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
   ) {
     this.x = x;
     this.y = y;
--- src/sandbox/index.ts
+++ src/sandbox/index.ts
@@ -1,4 +1,3 @@
-import { randomInt } from 'pulsar-pathfinding';
 import { Vector3 } from '../core/Vector3';
 import { Mesh } from '../core/mesh/Mesh';
 import { scene, updater } from './app';
@@ -12,24 +11,18 @@ const renderGameObject = new RenderGameObject();
 renderGameObject.addComponent(new RenderLoop());
 
 const loadPromises: Array<Promise<Mesh>> = [];
-for (let i = 0; i < 150; i++) {
-  const teddy = new Teddy();
-  loadPromises.push(teddy.loadMesh());
+for (let i = 0; i < 20; i++) {
+  loadPromises.push(new Teddy().loadMesh());
 }
 
 Promise.all(loadPromises).then((meshes) =>
   meshes.forEach((mesh, index) => {
     const radius = 300;
+    const zOffset = 500;
     const angle = (index * Math.PI * 2) / meshes.length;
     const x = Math.cos(angle) * radius;
-    const z = Math.sin(angle) * radius;
-    const sf = 2;
-    mesh.transforms.scale = new Vector3({ x: sf, y: sf, z: sf });
-    mesh.transforms.translation = new Vector3({
-      x,
-      y: randomInt(-100, 100),
-      z: z - 500,
-    });
+    const z = Math.sin(angle) * radius - zOffset;
+    mesh.transforms.translation = new Vector3({ x, z });
     scene.add(mesh);
   }),
 );
