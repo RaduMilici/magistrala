6c1c8dc22a15c92d14b91b6dfb6fcebd774fe022
Radu Milici <radu.milici@3pillarglobal.com>
2021-11-04 17:37:18 +0200

    destructuring invertedMatrix

--- src/core/scene/Scene.ts
+++ src/core/scene/Scene.ts
@@ -10,9 +10,9 @@ export class Scene {
     this.children.push(child);
   }
 
-  render(context: WebGL2RenderingContext, camera: Camera) {
+  render(context: WebGL2RenderingContext, { invertedMatrix }: Camera) {
     this.children.forEach((child) => {
-      child.prepareForRender(camera.invertedMatrix);
+      child.prepareForRender(invertedMatrix);
       context.drawArrays(WebGL2RenderingContext.TRIANGLES, 0, child.vertCount);
     });
   }
